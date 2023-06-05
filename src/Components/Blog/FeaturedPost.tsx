1a2712d168957e890dfabab1174c05c5c0b2a439
samuel <proftoby97@gmail.com>
2023-06-05 10:04:42 +0100

    updated prod

--- src/Components/Blog/FeaturedPost.tsx
+++ src/Components/Blog/FeaturedPost.tsx
@@ -30,7 +30,7 @@ const FeaturedPost : React.FC<FeaturedPostProps>= ({post}) => {
         <CardMedia
             component="img"
             sx={{ width: '100%', height:'200px', display: { xs: 'none', sm: 'block' } }}
-            image={post.image === '' ? 'https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' : post.image}
+            image={post?.image === '' ? 'https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' : post.image}
             alt={''}
           />
           <CardContent sx={{ flex: 1 }}>
@@ -38,7 +38,7 @@ const FeaturedPost : React.FC<FeaturedPostProps>= ({post}) => {
               {truncate(title, isMobile ? 14 :  20)}
             </Typography>
             <Typography variant="subtitle1" color="text.secondary">
-              {getLocaltime(createdAt)}
+              {getLocaltime(post?.createdAt)}
             </Typography>
             <Typography variant="subtitle1" paragraph sx={{minHeight: 100, maxHeight: 100}}>
               {truncate(post.description, 100)}
