e9bb9467dfb1afd539d3d29d98fe36c56d98c177
profsam97 <proftoby4real@hotmail.com>
2023-10-23 10:36:01 +0100

    updated baseurl

--- src/Helpers/BaseUrl.ts
+++ src/Helpers/BaseUrl.ts
@@ -2,6 +2,6 @@ const production : string = 'https://eye-server.up.railway.app';
 
 const develop : string = 'http://eye-server:5000';
 
-const baseurl : string = process.env.NEXT_PUBLIC_SERVER || 'http://192.168.33.10';
+const baseurl : string = process.env.NEXT_PUBLIC_SERVER || 'http://localhost:5000';
 
 export default baseurl;
