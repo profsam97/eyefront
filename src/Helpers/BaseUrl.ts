dd1c26dec261e9057f0658a5566dbfd59db13aba
profsam97 <proftoby4real@hotmail.com>
2023-11-08 14:18:36 +0100

    updated server url

--- src/Helpers/BaseUrl.ts
+++ src/Helpers/BaseUrl.ts
@@ -2,7 +2,9 @@ export const develop : string = 'http://web-server.default.svc.cluster.local:500
 
 const production : string = 'http://eye.vprofile.cloudradar.online';
 
-const baseurl : string = production;
+const baseurl : string =   production;
 
+
+export const server = process.env.NEXT_PUBLIC_SERVER || 'http://localhost:5000' 
 
 export default baseurl;
